50191929ff13e3385e2dbda03033ea393b32f2df
otalorajuand <otalorajuand@gmail.com>
2023-03-29 09:13:17 -0500

    Addition of task 0

--- insert_in_sorted_linked_list/0-insert_number.c
+++ insert_in_sorted_linked_list/0-insert_number.c
@@ -8,13 +8,18 @@ listint_t *insert_node(listint_t **head, int number)
 	listint_t *new;
 	listint_t *current;
 
-   	 current = *head;
+   	if (*head == NULL || **head == NULL)
+	{
+		return (NULL);
+	}
+
+	current = *head;
 
     	new = malloc(sizeof(listint_t));
     	if (new == NULL)
         	return (NULL);
 
-	while (current->next->n < number)
+	while (current->next->n < number || current->next != NULL)
 	{
 		current = current->next;
 	}
