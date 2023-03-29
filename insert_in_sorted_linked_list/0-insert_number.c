3d56f1f4e8f1ecf1054c555ea92ce972079bf6ee
otalorajuand <otalorajuand@gmail.com>
2023-03-29 09:14:25 -0500

    Addition of task 0

--- insert_in_sorted_linked_list/0-insert_number.c
+++ insert_in_sorted_linked_list/0-insert_number.c
@@ -8,7 +8,7 @@ listint_t *insert_node(listint_t **head, int number)
 	listint_t *new;
 	listint_t *current;
 
-   	if (*head == NULL || **head == NULL)
+   	if (*head == NULL || head == NULL)
 	{
 		return (NULL);
 	}
