74432f8897226aa4004e2020200c7f746d677145
otalorajuand <otalorajuand@gmail.com>
2023-04-06 10:24:25 -0500

    Addition of task 0

---
+++ heap_insert/0-binary_tree_node.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "binary_trees.h"
+
+
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value) {
+
+	binary_tree_t *new;
+
+	new = malloc(sizeof(binary_tree_t));
+    	if (new == NULL)
+        	return (NULL);
+	
+	new->n = value;
+	new->parent = parent;
+
+	return (new);
+}
+
