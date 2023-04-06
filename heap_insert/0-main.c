0ffb8908b3aeb3fa20449611693baebce2247c14
otalorajuand <otalorajuand@gmail.com>
2023-04-06 10:41:30 -0500

    Addition of task 0

--- heap_insert/0-main.c
+++ heap_insert/0-main.c
@@ -1,4 +1,5 @@
 #include <stdlib.h>
+#include <stdio.h>
 #include "binary_trees.h"
 
 /**
@@ -23,19 +24,18 @@ static void _binary_tree_delete(binary_tree_t *tree)
  */
 int main(void)
 {
-    binary_tree_t *root;
+	binary_tree_t *root;
 
-    root = binary_tree_node(NULL, 98);
-
-    root->left = binary_tree_node(root, 12);
-    root->left->left = binary_tree_node(root->left, 6);
-    root->left->right = binary_tree_node(root->left, 16);
-
-    root->right = binary_tree_node(root, 402);
-    root->right->left = binary_tree_node(root->right, 256);
-   root->right->right = binary_tree_node(root->right, 512);
-
-    binary_tree_print(root);
-    _binary_tree_delete(root);
-    return (0);
-} 
+	root = binary_tree_node(NULL, 180);
+	if (!root)
+	{
+		printf("binary_tree_node returned NULL\n");
+		return (1);
+	}
+	printf("Parent:%p\n", (void *)(root->parent));
+	printf("Left:%p\n", (void *)(root->left));
+	printf("Right:%p\n", (void *)(root->right));
+	binary_tree_print(root);
+	_binary_tree_delete(root);
+	return (0);
+}
