9f47bdd3a86ca8872389e5ac69a1ac9d49c9ad2f
otalorajuand <otalorajuand@gmail.com>
2023-04-20 16:54:57 -0500

    Addition of task 0

--- sandpiles/0-sandpiles.c
+++ sandpiles/0-sandpiles.c
@@ -9,19 +9,19 @@
  */
 static void print_grid(int grid[3][3])
 {
-    int i, j;
+	int i, j;
 
-    printf("=\n");
-    for (i = 0; i < 3; i++)
-    {
-        for (j = 0; j < 3; j++)
-        {
-            if (j)
-                printf(" ");
-            printf("%d", grid[i][j]);
-        }
-        printf("\n");
-    }
+	printf("=\n");
+	for (i = 0; i < 3; i++)
+	{
+		for (j = 0; j < 3; j++)
+		{
+			if (j)
+                		printf(" ");
+			printf("%d", grid[i][j]);
+		}
+		printf("\n");
+	}
 }
 
 /**
@@ -33,58 +33,58 @@ static void print_grid(int grid[3][3])
  */
 void sandpiles_sum(int grid1[3][3], int grid2[3][3])
 {
-    int i, j, unstable;
-    int tmp[3][3];
+	int i, j, unstable;
+	int tmp[3][3];
 
-    for (i = 0; i < 3; i++)
-    {
-        for (j = 0; j < 3; j++)
-        {
-            grid1[i][j] += grid2[i][j];
-        }
-    }
-    do {
-        unstable = 0;
+	for (i = 0; i < 3; i++)
+	{
+		for (j = 0; j < 3; j++)
+		{
+			grid1[i][j] += grid2[i][j];
+		}
+	}
+	do {
+		unstable = 0;
 
-        for (i = 0; i < 3; i++)
-        {
-            for (j = 0; j < 3; j++)
-            {
-                tmp[i][j] = grid1[i][j];
-            }
-        }
+		for (i = 0; i < 3; i++)
+		{
+			for (j = 0; j < 3; j++)
+			{
+				tmp[i][j] = grid1[i][j];
+			}
+		}
 
-        for (i = 0; i < 3; i++)
-        {
-            for (j = 0; j < 3; j++)
-            {
-                if (tmp[i][j] > 3)
-                {
-                    unstable = 1;
-                }
-            }
-        }
+		for (i = 0; i < 3; i++)
+		{
+			for (j = 0; j < 3; j++)
+			{
+				if (tmp[i][j] > 3)
+				{
+					unstable = 1;
+				}
+			}
+		}
 	
-	if (unstable)
+		if (unstable)
 		print_grid(grid1);
 
-        for (i = 0; i < 3; i++)
-        {
-            for (j = 0; j < 3; j++)
-            {
-                if (tmp[i][j] > 3)
-                {
-                    grid1[i][j] -= 4;
-                    if (i > 0)
-                        grid1[i - 1][j] += 1;
-                    if (i < 2)
-                        grid1[i + 1][j] += 1;
-                    if (j > 0)
-                        grid1[i][j - 1] += 1;
-                    if (j < 2)
-                        grid1[i][j + 1] += 1;
-                }
-            }
-        }
-    } while (unstable);
+	for (i = 0; i < 3; i++)
+	{
+		for (j = 0; j < 3; j++)
+		{
+			if (tmp[i][j] > 3)
+			{
+				grid1[i][j] -= 4;
+				if (i > 0)
+					grid1[i - 1][j] += 1;
+				if (i < 2)
+					grid1[i + 1][j] += 1;
+				if (j > 0)
+					grid1[i][j - 1] += 1;
+				if (j < 2)
+					grid1[i][j + 1] += 1;
+			}
+		}
+	}
+	} while (unstable);
 }
