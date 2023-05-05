c16cb1419408d8f3d4e618783c3187b2247f692c
otalorajuand <otalorajuand@gmail.com>
2023-05-04 21:00:10 -0500

    Addition of task 0

--- log_parsing/0-stats.py
+++ log_parsing/0-stats.py
@@ -3,6 +3,13 @@
 import sys
 
 
+def print_stats(total_file_size, dic):
+    print("File size: {}".format(total_file_size))
+    for k in sorted(dic):
+        if dic[k] != 0:
+            print("{}: {}".format(k, dic[k]))
+
+
 
 line_counter = 0
 total_file_size = 0
@@ -15,14 +22,13 @@ status_codes = {'200': 0,
                 '405': 0,
                 '500': 0}
 
-def print_stats(total_file_size, dic):
-    print("File size: {}".format(total_file_size))
-    for k in sorted(dic):
-        if dic[k] != 0:
-            print("{}: {}".format(k, dic[k]))
-
 try: 
     for line in sys.stdin:
+
+        if line_counter != 0 and line_counter % 10 == 0:
+            print_stats(total_file_size, status_codes)
+ 
+
         line = line.rstrip()
  
         line_list = line.split(" ")
@@ -34,9 +40,7 @@ try:
         file_size = int(line_list[8])
         total_file_size += file_size
 
-        if line_counter != 0 and line_counter % 10 == 0:
-            print_stats(total_file_size, status_codes)
-        line_counter += 1
+       line_counter += 1
     print_stats(total_file_size, status_codes)
 except KeyboardInterrupt:
     print_stats(total_file_size, status_codes)
