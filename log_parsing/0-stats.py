0bbd081f2493d82a34ec305f59d0ba956a761ed2
otalorajuand <otalorajuand@gmail.com>
2023-05-04 21:05:29 -0500

    Addition of task 0

--- log_parsing/0-stats.py
+++ log_parsing/0-stats.py
@@ -14,21 +14,23 @@ status_codes = {'200': 0,
                 '405': 0,
                 '500': 0}
 
+
 def print_stats(total_file_size, dic):
     """Prints the stats"""
     print("File size: {}".format(total_file_size))
     for k in sorted(dic):
         if dic[k] != 0:
             print("{}: {}".format(k, dic[k]))
-try: 
+
+
+try:
     for line in sys.stdin:
 
         if line_counter != 0 and line_counter % 10 == 0:
             print_stats(total_file_size, status_codes)
- 
 
         line = line.rstrip()
- 
+
         line_list = line.split(" ")
         status_code = str(line_list[7])
 
@@ -38,7 +40,7 @@ try:
         file_size = int(line_list[8])
         total_file_size += file_size
 
-       line_counter += 1
+        line_counter += 1
     print_stats(total_file_size, status_codes)
 except KeyboardInterrupt:
     print_stats(total_file_size, status_codes)
