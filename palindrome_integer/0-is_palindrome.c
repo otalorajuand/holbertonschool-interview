95b201fd615d901b0df1f393761b747346654e70
otalorajuand <otalorajuand@gmail.com>
2023-05-18 11:24:04 -0500

    Addition of task 0

---
+++ palindrome_integer/0-is_palindrome.c
@@ -0,0 +1,53 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+
+#include "palindrome.h"
+
+int reverse_number(unsigned long n)
+{
+    int reversed_number = 0, remainder;
+
+    while(n != 0) {
+        remainder = n % 10;
+        reversed_number = reversed_number * 10 + remainder;
+        n /= 10;
+    }
+
+    return(reversed_number);
+}
+
+int power(int first, int second)
+{
+    int i, res = 1;
+    for (i = 0; i<second; i++)
+    {
+        res *= first;
+    }
+
+    return(res);
+}
+
+int is_palindrome(unsigned long n)
+{
+    int count = 0, den;
+    unsigned long aux = n, first, last;
+    //int last, first;
+
+    while (aux != 0)
+    {
+        count++;
+        aux = aux / 10;
+    }
+
+    den = power(10, count/2);
+
+    first = count%2 == 0 ? n/den : n/(den * 10);
+
+    last = reverse_number(n%den);
+
+    if (first == last)
+        return (1);
+
+    return(0);
+}
---
+++ palindrome_integer/palindrome.h
@@ -0,0 +1,9 @@
+#ifndef PALINDROME_H
+#define PALINDOMRE_H
+
+
+int is_palindrome(unsigned long n);
+int reverse_number(unsigned long n);
+int power(int first, int second);
+
+#endif /* PALINDROME_H */
