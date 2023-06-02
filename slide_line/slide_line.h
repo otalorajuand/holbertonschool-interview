5d03e35b37cdd652d669a781ab43fcdd89ba1e99
otalorajuand <otalorajuand@gmail.com>
2023-06-02 15:10:24 -0500

    Addition of task 0

--- slide_line/slide_line.h
+++ slide_line/slide_line.h
@@ -4,9 +4,11 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-#define SLIDE_LEFT  0
+#define SLIDE_LEFT 0
 #define SLIDE_RIGHT 1
+#define LINE_SIZE 2048
 
 int slide_line(int *line, size_t size, int direction);
 
 #endif /* SLIDE_LINE_H */
+
