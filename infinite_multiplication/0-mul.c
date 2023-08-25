303faa851de9f89a20719b507bf8426717a543cc
otalorajuand <otalorajuand@gmail.com>
2023-08-25 10:20:20 -0500

    Addition of task 0

--- infinite_multiplication/0-mul.c
+++ infinite_multiplication/0-mul.c
@@ -7,13 +7,14 @@ void print_error() {
 }
 
 int main(int argc, char *argv[]) {
+    unsigned long num1, num2;
     int i;
 
     if (argc != 3) {
         print_error();
     }
 
-    // Check if num1 and num2 are composed of digits
+
     for (i = 0; argv[1][i] != '\0'; i++) {
         if (argv[1][i] < '0' || argv[1][i] > '9') {
             print_error();
@@ -25,14 +26,14 @@ int main(int argc, char *argv[]) {
         }
     }
 
-    // Convert num1 and num2 to integers
-    unsigned long num1 = strtoul(argv[1], NULL, 10);
-    unsigned long num2 = strtoul(argv[2], NULL, 10);
 
-    // Perform multiplication
+    num1 = strtoul(argv[1], NULL, 10);
+    num2 = strtoul(argv[2], NULL, 10);
+
+
     unsigned long result = num1 * num2;
 
-    // Print the result
+
     printf("%lu\n", result);
 
     return 0;
