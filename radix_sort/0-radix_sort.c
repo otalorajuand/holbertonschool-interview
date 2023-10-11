ad8d33d9ad38490f95ba5180aa38eaf211d804e6
otalorajuand <otalorajuand@gmail.com>
2023-10-10 19:48:46 -0500

    Addition of task 0

--- radix_sort/0-radix_sort.c
+++ radix_sort/0-radix_sort.c
@@ -2,10 +2,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/* Function to get the maximum value in the array */
 int getMax(int *array, size_t size) {
     int max = array[0];
-    for (size_t i = 1; i < size; i++) {
+    size_t i;
+
+    for (i = 1; i < size; i++) {
         if (array[i] > max) {
             max = array[i];
         }
@@ -13,45 +14,48 @@ int getMax(int *array, size_t size) {
     return max;
 }
 
-/* Counting sort for a specific digit (exp) */
 void countSort(int *array, size_t size, int exp) {
     int *output = malloc(size * sizeof(int));
     int count[10] = {0};
+    size_t i;
 
     if (output == NULL) {
         return;
     }
 
-    for (size_t i = 0; i < size; i++) {
+    for (i = 0; i < size; i++) {
         count[(array[i] / exp) % 10]++;
     }
 
-    for (int i = 1; i < 10; i++) {
+    for (i = 1; i < 10; i++) {
         count[i] += count[i - 1];
     }
 
-    for (int i = size - 1; i >= 0; i--) {
+    for (i = size - 1; i < size; i--) {
         output[count[(array[i] / exp) % 10] - 1] = array[i];
         count[(array[i] / exp) % 10]--;
     }
 
-    for (size_t i = 0; i < size; i++) {
+    for (i = 0; i < size; i++) {
         array[i] = output[i];
     }
 
     free(output);
 }
 
-/* Radix sort */
 void radix_sort(int *array, size_t size) {
     if (array == NULL || size < 2) {
         return;
     }
 
     int max = getMax(array, size);
+    int exp;
+    size_t i;
 
-    for (int exp = 1; max / exp > 0; exp *= 10) {
+    for (exp = 1; max / exp > 0; exp *= 10) {
+        for (i = 0; i < size; i++) { // Move the loop variable declaration here
+            print_array(array, size);
+        }
         countSort(array, size, exp);
-        print_array(array, size);
     }
 }
