9f907b940ccaf6583260bdd159dd2d329ae14a2b
otalorajuand <otalorajuand@gmail.com>
2023-10-10 20:00:25 -0500

    Addition of task 0

--- radix_sort/0-radix_sort.c
+++ radix_sort/0-radix_sort.c
@@ -2,6 +2,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
+
+/**
+ * getMax - Find the maximum element in an array
+ *
+ * @array: The input array
+ * @size: Size of the array
+ *
+ * Return: The maximum element in the array
+ */
 int getMax(int *array, size_t size) {
     int max = array[0];
     size_t i;
@@ -14,6 +24,14 @@ int getMax(int *array, size_t size) {
     return max;
 }
 
+
+/**
+ * countSort - Perform counting sort on an array based on a significant digit
+ *
+ * @array: The input array
+ * @size: Size of the array
+ * @exp: The current significant digit (e.g., 1, 10, 100, ...)
+ */
 void countSort(int *array, size_t size, int exp) {
     int *output = malloc(size * sizeof(int));
     int count[10] = {0};
@@ -43,13 +61,20 @@ void countSort(int *array, size_t size, int exp) {
     free(output);
 } 
 
+
+/**
+ * radix_sort - Sort an array of integers in ascending order using Radix sort
+ *
+ * @array: The input array
+ * @size: Size of the array
+ */
 void radix_sort(int *array, size_t size) {
 
     int max;
     int exp;
 
     if (array == NULL || size < 2) {
-        return;
+    return;
     }
 
     max = getMax(array, size);
