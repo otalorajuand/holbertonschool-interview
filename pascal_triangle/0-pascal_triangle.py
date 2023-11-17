3c4773e8cca1bfd14f828fec8923c81a42425937
otalorajuand <otalorajuand@gmail.com>
2023-11-17 11:17:34 -0500

    Addition of task 0

--- pascal_triangle/0-pascal_triangle.py
+++ pascal_triangle/0-pascal_triangle.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
 """returns a list of lists of integers representing
    the Pascal's triangle of n"""
-   
+
 
 def pascal_triangle(n):
+    """returns a list of lists of integers representing
+   the Pascal's triangle of n"
+    """
     if n <= 0:
         return []
 
